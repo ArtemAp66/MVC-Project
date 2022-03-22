97d4dbc80476693cd47741df412cb217081d1a1d
ArtemAp66 <artemstarovoitovap@gmail.com>
2022-03-22 13:13:46 +0500

    Version 1.0.5

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+#Открываем сессию в начале программы!
+
 # Подгрузка конфигурации проекта
 include_once 'config.php';
 # Подключаем автозагрузчик
 include_once "autoload.php";
+include_once "core/basefunction.php";
 include_once "core/view.php";
 # Подключаем маршрутизацию
 include_once "route.php";
